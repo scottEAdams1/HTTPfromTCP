8dd2a66047b62cd676296af6fa7b9e658f2ff625
scottEAdams1 <scott.e.adams01@gmail.com>
2025-06-15 19:51:30 +0100

    Added video handling, and refactored proxy handling

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
-	"HTTPfromTCP/internal/headers"
 	"HTTPfromTCP/internal/request"
 	"HTTPfromTCP/internal/response"
 	"HTTPfromTCP/internal/server"
+	"crypto/sha256"
+	"errors"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
 	"syscall"
 )
 
@@ -30,26 +35,57 @@ func main() {
 
 func serverHandler(w *response.Writer, req *request.Request) {
 	path := req.RequestLine.RequestTarget
-	var statusCode response.StatusCode
-	var headers headers.Headers
-	var message []byte
-	if path == "/yourproblem" {
-		statusCode = response.BADREQUEST
-		message = []byte("<html><head><title>400 Bad Request</title></head><body><h1>Bad Request</h1><p>Your request honestly kinda sucked.</p></body></html>")
+	if strings.HasPrefix(req.RequestLine.RequestTarget, "/httpbin") {
+		proxyHandler(w, req)
+		return
+	} else if path == "/video" {
+		videoHandler(w, req)
+		return
+	} else if path == "/yourproblem" {
+		handler400(w, req)
+		return
 	} else if path == "/myproblem" {
-		statusCode = response.SERVERERROR
-		message = []byte("<html><head><title>500 Internal Server Error</title></head><body><h1>Internal Server Error</h1><p>Okay, you know what? This one is on me.</p></body></html>")
+		handler500(w, req)
+		return
 	} else {
-		statusCode = response.OK
-		message = []byte("<html><head><title>200 OK</title></head><body><h1>Success!</h1><p>Your request was an absolute banger.</p></body></html>")
+		handler200(w, req)
+		return
 	}
-	headers = w.GetDefaultHeaders(len(message))
-	err := w.WriteStatusLine(statusCode)
+}
+
+func handler200(w *response.Writer, req *request.Request) {
+	err := w.WriteStatusLine(response.OK)
 	if err != nil {
 		fmt.Println("Error writing status line:", err.Error())
 		return
 	}
-	err = w.WriteHeaders(headers)
+	message := []byte("<html><head><title>200 OK</title></head><body><h1>Success!</h1><p>Your request was an absolute banger.</p></body></html>")
+	handlerDefault(w, message)
+}
+
+func handler400(w *response.Writer, req *request.Request) {
+	err := w.WriteStatusLine(response.BADREQUEST)
+	if err != nil {
+		fmt.Println("Error writing status line:", err.Error())
+		return
+	}
+	message := []byte("<html><head><title>400 Bad Request</title></head><body><h1>Bad Request</h1><p>Your request honestly kinda sucked.</p></body></html>")
+	handlerDefault(w, message)
+}
+
+func handler500(w *response.Writer, req *request.Request) {
+	err := w.WriteStatusLine(response.SERVERERROR)
+	if err != nil {
+		fmt.Println("Error writing status line:", err.Error())
+		return
+	}
+	message := []byte("<html><head><title>500 Internal Server Error</title></head><body><h1>Internal Server Error</h1><p>Okay, you know what? This one is on me.</p></body></html>")
+	handlerDefault(w, message)
+}
+
+func handlerDefault(w *response.Writer, message []byte) {
+	headers := w.GetDefaultHeaders(len(message))
+	err := w.WriteHeaders(headers)
 	if err != nil {
 		fmt.Println("Error writing headers:", err.Error())
 		return
@@ -59,4 +95,50 @@ func serverHandler(w *response.Writer, req *request.Request) {
 		fmt.Printf("Error writing body after %v bytes: %s", n, err.Error())
 		return
 	}
+}
+
+func proxyHandler(w *response.Writer, req *request.Request) {
+	w.WriteStatusLine(response.OK)
+	headers := w.GetDefaultHeaders(0)
+	delete(headers, "Content-Length")
+	headers["Transfer-Encoding"] = "chunked"
+	headers["Trailer"] = "X-Content-SHA256,X-Content-Length"
+	w.WriteHeaders(headers)
+	res, err := http.Get("https://httpbin.org" + strings.TrimPrefix(req.RequestLine.RequestTarget, "/httpbin"))
+	if err != nil {
+		return
+	}
+	var full []byte
+	for true {
+		b := make([]byte, 1024)
+		n, err := res.Body.Read(b)
+		full = append(full, b[:n]...)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				if n > 0 {
+					w.WriteChunkedBody(b[:n])
+				}
+				hash := fmt.Sprintf("%x", sha256.Sum256(full))
+				headers["X-Content-SHA256"] = hash
+				headers["X-Content-Length"] = strconv.Itoa(len(full))
+				w.WriteTrailers(headers)
+				break
+			}
+			return
+		}
+		w.WriteChunkedBody(b[:n])
+	}
+}
+
+func videoHandler(w *response.Writer, req *request.Request) {
+	data, err := os.ReadFile("assets/vim.mp4")
+	if err != nil {
+		handler500(w, req)
+		return
+	}
+	w.WriteStatusLine(response.OK)
+	headers := w.GetDefaultHeaders(len(data))
+	headers["Content-Type"] = "video/mp4"
+	w.WriteHeaders(headers)
+	w.WriteBody(data)
 }
